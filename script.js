fcae0c94e85f894cb06fc967d0551622abd567cf
Muhammaddavron <muhammaddavron123@gmail.com>
2024-08-09 22:16:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let a = prompt('yoshingizni kiriting')
+if(0>a){
+alert('Hali tugilmapsanku bolam')
+}
+
+else if(18>a>0){
+alert('Ukam soskeni yoqotib qoydinmi?')
+}else{
+    alert('Assalomu aleykum Va Rahmatullohi va Barakatuh ozimni qadrdon akam')
+}
